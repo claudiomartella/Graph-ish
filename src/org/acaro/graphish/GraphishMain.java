b3d69565660cc4eaeb5d4819596c5abcc0e7df86
Claudio Martella <claudio.martella@gmail.com>
2010-12-13 22:24:56 +0100

    cleaned up GraphishMain

--- src/org/acaro/graphish/GraphishMain.java
+++ src/org/acaro/graphish/GraphishMain.java
@@ -1,23 +1,9 @@
 package org.acaro.graphish;
 
-import no.ntnu.idi.sgdb.SGDB;
-import no.ntnu.idi.sgdb.SGDBException;
-import no.ntnu.idi.sgdb.service.SgdbPersistentBDBServiceImple;
-
 public class GraphishMain {
 
-	/**
-	 * @param args
-	 */
 	public static void main(String[] args) {
-		System.out.println("Welcome to Graphandra v0.1");
-		String path = "./bdb";
-		try {
-			SgdbPersistentBDBServiceImple s = (SgdbPersistentBDBServiceImple) SGDB
-					.createPersistentSgdbService(path, false, SGDB.BDB);
-		} catch (SGDBException e) {
-			e.printStackTrace();
-		}
+		System.out.println("Welcome to Graph-ish v0.1");
+		Graphish graph = new Graphish(new HBaseStore());
 	}
-
 }
